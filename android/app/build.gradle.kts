5f01bea03379def4c241fea40ede51d843ae20d7
pin0n0 <brumarti@umag.cl>
2025-12-12 17:54:38 -0300

    actualizacion de depencias para utilizar qr

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -13,6 +13,8 @@ android {
     compileOptions {
         sourceCompatibility = JavaVersion.VERSION_11
         targetCompatibility = JavaVersion.VERSION_11
+        // Enable core library desugaring required by some AARs (eg. qr_code_scanner)
+        isCoreLibraryDesugaringEnabled = true
     }
 
     kotlinOptions {
@@ -41,4 +43,9 @@ android {
 
 flutter {
     source = "../.."
+}
+
+dependencies {
+    // Required to support Java 8+ APIs in libraries that need desugaring (e.g. qr_code_scanner)
+    coreLibraryDesugaring("com.android.tools:desugar_jdk_libs:2.0.3")
 }
